9aaccbe8eb46283ff569aff8a48049dab8de1bba
jozwright <jozwright@gmail.com>
2014-08-11 20:56:02 -0700

    assumes user will enter input as a string

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,10 +1,10 @@
 def luck_check(ticket_input)
 
-	ticket_nums = ticket_input.to_s.split('').map { |num| num.to_i }
+	ticket_nums = ticket_input.split('').map { |num| num.to_i }
 
 	half = ticket_nums.length / 2
 
-	counter = 0
+	first_sum = 0
 	ticket_nums.first(half).each { |num| first_sum += num }
 
 	last_sum = 0
