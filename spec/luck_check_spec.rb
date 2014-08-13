a8a195a20b908d587404a770011c597e522b3ca6
jozwright <jozwright@gmail.com>
2014-08-13 12:03:02 -0700

    full spec coverage for uneven and even tickets

--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -2,10 +2,16 @@ require 'rspec'
 require 'luck_check'
 
 describe('luck_check') do
-	it('returns true if the ticket length is even and the sum of digits on the left half equal the sum of the digits on the right half') do
+	it('returns true if the ticket length is even and the sums of both halves are equal') do
 		luck_check('5041').should(eq(true))
 	end
-	it('returns true if the ticket length is uneven and the sum of digits on the left half equal the sum of the digits on the right half') do
-    	luck_check('5041').should(eq(true))
+	it('returns false if ticket length is even and the sums of both halves are unequal') do
+    	luck_check('5141').should(eq(false))
+  	end
+  	it('returns true if the ticket length is uneven and the sums of both halves are equal') do
+		luck_check('50241').should(eq(true))
+	end
+	it('returns false if ticket length is uneven and the sums of both halves are unequal') do
+    	luck_check('51941').should(eq(false))
   	end
 end
